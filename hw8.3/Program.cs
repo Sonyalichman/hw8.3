46941a0626cfb31405a650ecaddeb4febe488e6d
Sofiia <sofiialichman@gmail.com>
2022-12-26 00:44:59 +0200

    Добавьте файлы проекта.

---
+++ hw8.3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Rectangle
+{
+    public virtual int Width { get; set; }
+    public virtual int Height { get; set; }
+    public int GetRectangleArea()
+    {
+        return Width * Height;
+    }
+}
+class Square : Rectangle
+{
+    public override int Width
+    {
+        get { return base.Width; }
+        set
+        {
+            base.Height = value;
+            base.Width = value;
+        }
+    }
+    public override int Height
+    {
+        get { return base.Height; }
+        set
+        {
+            base.Height = base.Width;
+            base.Width = base.Width;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Square();
+            rect.Width = 5;
+            Console.WriteLine(rect.GetRectangleArea());
+            Console.ReadKey();
+        }
+    }
+}
